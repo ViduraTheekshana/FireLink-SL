a7c9662e9c6ac7c0c850764a4824276aacd5853c
isiwari2004 <isiwarithimethya@gmail.com>
2025-10-09 18:44:21 +0530

    edit miss frontend

--- frontend/src/pages/MissionRecords/MissionRecords.jsx
+++ frontend/src/pages/MissionRecords/MissionRecords.jsx
@@ -363,7 +363,7 @@ const MissionRecords = () => {
 
       
                                 if (!value.startsWith("IT")) {
-                                   value = "ID" + value.replace(/^ID/, "");
+                                   value = "ID" + value.replace(/^ID/, "");//edit this
                                     }
 
     
